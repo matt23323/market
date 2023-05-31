1e2ca3d5cf5277288362d8ef61db4dd88138d7de
matt23323 <chebyrek543@gmail.com>
2023-05-31 13:47:50 +0100

    dsadasd

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-// $.ajax('https://my-json-server.typicode.com/matt23323/matt23323'),{
+let grid = document.getElementById("products-grid")
+let productsArray = []
+let xhr = new XMLHttpRequest();
+let url = "https://my-json-server.typicode.com/matt23323/market"
 
-// }
+
+xhr.open("GET",url+"/products")
+xhr.responseType = "json"
+xhr.onload = function(){
+    productsArray = xhr.response
+    grid =  null;
+    productsArray.forEach(p=> {
+        productsArray.push(p)
+    let product = document.createElement("div")
+    product.classList.add("product");
+    product.innerHTML = `
+    <h1 class="product-name">${p.name}</h1>
+    <img class="product-photo" src="${p.photo_url}">
+    <p class="product-price">Price:${p.price}</p>
+    <p class="product-description">descript:${p.description}</p>
+    <a href="userProfile.html?id=${p.author_id}>Seller profile</a>
+    <button onclick="addProductToCart(${p.id})">Buy</button>
+    `;
+    grid.append(product);
+})
+}
+xhr.send();
