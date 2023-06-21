86c5d2435d4bd59721902fb02da857b6ac6851cb
matt23323 <chebyrek543@gmail.com>
2023-06-21 12:03:15 +0100

    sadasd

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let productsArray = []
 let xhr = new XMLHttpRequest();
 let url = "https://my-json-server.typicode.com/matt23323/market"
 
+let a = document.getElementById("cart-button")
+let b = document.getElementById("cart-products")
+let cart = []
 
 xhr.open("GET",url+"/products")
 xhr.responseType = "json"
@@ -19,9 +22,56 @@ xhr.onload = function(){
     <p class="product-price">Price: ${p.price}</p>
     <p class="product-description">Description: ${p.description}</p>
     <a href="userProfile.html?id=${p.author_id}">Seller profile</a>
-    <button onclick="addProductToCart"(${p.id})">Buy</button>
+    <button onclick="addProductToCart()"(${p.id})">Buy</button>
     `;
     grid.append(product);
 })
 }
 xhr.send();
+
+
+function openCart(){
+    cartProd.classList.toggle('hide')
+}
+function closeCart(){
+    cartProd.classList.toggle('hide')
+}
+let cartProd = document.getElementById('cart-products');
+// if(localStorage.getItem('cart')) {
+//     cart = JSON.parse(localStorage.getItem('cart'));
+//     drawCartProducts();
+// }
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p.id == id;
+    })
+    cart.push(product);
+    // drawCartProducts();
+    // localStorage.setItem("cart", JSON.stringify(cart));
+
+    // document.getElementById('cart-button').classList.add('active');
+    // setTimeout(function(){
+    //     document.getElementById('cart-button').classList.remove('active');
+    // },500);
+}
+function drawCartProducts() {
+    if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.price;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    localStorage.setItem("cart", '[]');
+}
